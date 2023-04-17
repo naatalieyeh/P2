9abd11177c8bb59ef7bda51a649ea9fa2d98aa45
nmyeh@purdue.edu <nmyeh@purdue.edu>
2023-04-17 14:18:54 -0400

    adding the model import file

---
+++ model_import.R
@@ -0,0 +1,16 @@
+install.packages("tidyverse")
+install.packages("keras")
+install.packages("tensorflow")
+install.packages("reticulate")
+library("tidyverse")
+library("keras")
+library("tensorflow")
+library("reticulate")
+install_tensorflow(extra_packages = "pillow")
+install_keras()
+setwd("/Users/natalieyeh/Desktop/P2")
+model <- load_model_tf("./dandelion_model")
+
+# hi
+
+# test branch change
