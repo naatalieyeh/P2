1fecf069e7a17d5f7825e471385b4813aaaadff9
nmyeh@purdue.edu <nmyeh@purdue.edu>
2023-04-25 15:56:56 -0400

    adding R changes + adversarial patch image

--- adversarialPatch.R
+++ adversarialPatch.R
@@ -1,1 +1,20 @@
-# seeing if i can commit this to the new branch
+width <- 224
+height<- 224
+target_size <- c(width, height)
+rgb <- 3 #color channels
+
+res=c("","")
+grassUnmodified=list.files("/Users/natalieyeh/Desktop/P2\ Model\ Setup/grass")
+for (i in grassUnmodified){
+  test_image <- image_load(paste("/Users/natalieyeh/Desktop/P2\ Model\ Setup/grass/",i,sep=""),
+                           target_size = target_size)
+  x <- image_to_array(test_image)
+  x <- array_reshape(x, c(1, dim(x)))
+  x <- x/255
+  pred <- model %>% predict(x)
+  if(pred[1,1]<0.50){
+    print(i)
+  }
+}
+
+install.package("magick")
