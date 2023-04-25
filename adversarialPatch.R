5196c557c1a458d23acc9e4bbed6661579a2d7f9
nmyeh@purdue.edu <nmyeh@purdue.edu>
2023-04-25 17:49:54 -0400

    worked on adversarial patch

--- adversarialPatch.R
+++ adversarialPatch.R
@@ -1,20 +1,53 @@
-width <- 224
-height<- 224
-target_size <- c(width, height)
-rgb <- 3 #color channels
+# width <- 224
+# height<- 224
+# target_size <- c(width, height)
+# rgb <- 3
+# 
+# install.packages("magick")
+# library("magick")
+# 
+# patch <- image_read("/Users/natalieyeh/Desktop/P2/patch.png")
+# patchResize <- image_scale(patch, "22.4")
+# patch <- patchResize
+# 
+# # grassPictures <- list.files("/Users/natalieyeh/Desktop/P2/grass")
+# # for (i in grassPictures) {
+# #   testImage <- image_load(paste("/Users/natalieyeh/Desktop/P2/grass/", i, sep = ""), target_size = c(224, 224))
+# #   x <- image_to_array(test_image)
+# #   
+# # }
+# 
+# 
+# test_image <- image_read(paste("/Users/natalieyeh/Desktop/P2/grass",1))
+# png('test.png', width = 2, height = 2, units = 'in', res = 150)
+# par(mai=c(0,0,0,0))
+# plot.new()
+# rasterImage(test_image, 0, 0, 1, 1)
+# rasterImage(img.logo,   0, 0, 1, 1)
+# dev.off()
 
-res=c("","")
-grassUnmodified=list.files("/Users/natalieyeh/Desktop/P2\ Model\ Setup/grass")
-for (i in grassUnmodified){
-  test_image <- image_load(paste("/Users/natalieyeh/Desktop/P2\ Model\ Setup/grass/",i,sep=""),
-                           target_size = target_size)
-  x <- image_to_array(test_image)
-  x <- array_reshape(x, c(1, dim(x)))
-  x <- x/255
-  pred <- model %>% predict(x)
-  if(pred[1,1]<0.50){
-    print(i)
-  }
-}
+install.packages("tidyverse")
+install.packages("keras")
+install.packages("tensorflow")
+install.packages("reticulate")
+library("tidyverse")
+library("keras")
+library("tensorflow")
+library("reticulate")
+install_tensorflow(extra_packages = "pillow")
+install_keras()
 
-install.package("magick")
+install.packages("magick")
+library("magick")
+
+test_image <- image_read(paste("/Users/natalieyeh/Desktop/P2/dandelions/",i,sep=""))
+test_imagePNG <- image_convert(test_image, "png")
+test_imagePNG <- image_scale(test_image, "224x224!")
+test_image <- test_imagePNG
+
+patch <- image_read("/Users/natalieyeh/Desktop/P2/patch.png")
+patchResize <- image_scale(patch, "22.4")
+patch <- patchResize
+
+image <- c(test_image, patch)
+image_composite(test_image, patch, offset = "+112+112")
