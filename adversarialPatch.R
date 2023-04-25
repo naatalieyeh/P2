8ecf9260b514ef80a3a198a6184546ab289a4dac
nmyeh@purdue.edu <nmyeh@purdue.edu>
2023-04-25 18:06:56 -0400

    added loop and turned back into jpgs

--- adversarialPatch.R
+++ adversarialPatch.R
@@ -9,27 +9,35 @@
 install.packages("magick")
 library("magick")
 
-test_image <- image_read(paste("/Users/natalieyeh/Desktop/P2/dandelions/",i,sep=""))
-test_imagePNG <- image_convert(test_image, "png")
-test_imagePNG <- image_scale(test_image, "224x224!")
-test_image <- test_imagePNG
+# test_image <- image_read(paste("/Users/natalieyeh/Desktop/P2/dandelions/",i,sep=""))
+# test_imagePNG <- image_convert(test_image, "png")
+# test_imagePNG <- image_scale(test_image, "224x224!")
+# test_image <- test_imagePNG
+# 
+# patch <- image_read("/Users/natalieyeh/Desktop/P2/patch.png")
+# patchResize <- image_scale(patch, "22.4")
+# patch <- patchResize
+# 
+# patchedImage <- image_composite(test_image, patch, offset = "+112+112")
 
-patch <- image_read("/Users/natalieyeh/Desktop/P2/patch.png")
-patchResize <- image_scale(patch, "22.4")
-patch <- patchResize
+dandelions <- list.files("/Users/natalieyeh/Desktop/P2/dandelions")
+dandelionsPatched <- c()
 
-patchedImage <- image_composite(test_image, patch, offset = "+112+112")
-
-grassPictures <- list.files("/Users/natalieyeh/Desktop/P2/dandelions")
-for (i in grassPictures) {
-  test_image <- image_read(paste("/Users/natalieyeh/Desktop/P2/dandelions/",i,sep=""))
-  test_imagePNG <- image_convert(test_image, "png")
-  test_imagePNG <- image_scale(test_image, "224x224!")
-  test_image <- test_imagePNG
+for (i in dandelions) {
+  testImage <- image_read(paste("/Users/natalieyeh/Desktop/P2/dandelions/",i,sep=""))
+  testImagePNG <- image_convert(testImage, "png")
+  testImagePNG <- image_scale(testImage, "224x224!")
+  testImage <- testImagePNG
   
   patch <- image_read("/Users/natalieyeh/Desktop/P2/patch.png")
   patchResize <- image_scale(patch, "22.4")
   patch <- patchResize
   
   patchedImage <- image_composite(test_image, patch, offset = "+112+112")
+  patchedImageJPG <- image_convert(patchedImage, "jpg")
+  patchedImage <- patchedImageJPG
+  dandelionsPatched <- c(dandelionsPatched, patchedImage)
 }
+
+
+
